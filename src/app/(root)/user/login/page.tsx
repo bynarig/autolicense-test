17b06995e89072a3fd1400497a7e72ae61c8b975
Yaroslav Shabelnyk <62596807+bynarig@users.noreply.github.com>
2025-04-12 15:13:55 +0200

    started DB implement and login logic

--- src/app/(root)/user/login/page.tsx
+++ src/app/(root)/user/login/page.tsx
@@ -1,38 +1,44 @@
-"use client"
-
+import {signIn} from "@/app/auth"
 import Navbar from "@/shared/ui/Navbar";
 import Footer from "@/shared/ui/Footer";
-import {useSelector} from "react-redux";
-import {RootState} from "@/shared/store";
 
 export default function Page() {
-    const isLogged = useSelector((state: RootState) => state.userSlice.isLogged);
+    // const isLogged = useSelector((state: RootState) => state.userSlice.isLogged);
+
     return (
         <>
             <Navbar/>
-            {
-                !isLogged ? <div className="flex justify-center h-140 mt-55">
-                        <div className="card bg-base-100 w-96 shadow-sm">
-                            <div className="flex justify-center">
-                                <h1 className="text-6xl">Login</h1>
-                            </div>
-                            <div className="card-body">
-                                <input type="text" placeholder="mail or username" className="input"/>
-                                <input type="text" placeholder="password" className="input"/>
-                                <button className="btn">Send</button>
-                            </div>
-                        </div>
-                    </div>
 
-                    : <div className="flex justify-center h-140 mt-70">
-                        <div className="card bg-base-100 w-140 shadow-sm">
-                            <div className="flex justify-center">
-                                <h1 className="text-4xl md:text-6xl">Already loggedIn 😵</h1>
-
-                            </div>
-                        </div>
+            <div className="flex justify-center h-140 mt-55">
+                <form className="card bg-base-100 w-96 shadow-sm" action={async () => {
+                    "use server"
+                    await signIn()
+                }}>
+                    <div className="flex justify-center">
+                        <h1 className="text-6xl">Login</h1>
                     </div>
-            }
+                    <div className="card-body">
+                        <input type="text" placeholder="mail or username" className="input"/>
+                        <input type="text" placeholder="password" className="input"/>
+                        <button className="btn" type="submit">Send</button>
+                        <button className="btn bg-white text-black border-[#e5e5e5]" type="submit" onClick={async () => {
+                            "use server"
+                            await signIn("google")
+                        }}>
+                            <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <g>
+                                    <path d="m0 0H512V512H0" fill="#fff"></path>
+                                    <path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path>
+                                    <path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path>
+                                    <path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path>
+                                    <path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path>
+                                </g>
+                            </svg>
+                            Login with Google
+                        </button>
+                    </div>
+                </form>
+            </div>
 
 
             <Footer/>
---
+++ src/shared/utils/password.ts
@@ -0,0 +1,12 @@
+// import bcrypt from "bcrypt";
+//
+// const saltRounds = 8;
+//
+//
+// export function saltAndHashPassword(password: string) {
+//     return bcrypt.hashSync(password, saltRounds);
+// }
+//
+// export function saltAndHashPasswordCompare(password: string, hash: string) {
+//     return bcrypt.compareSync(password, hash);
+// }
